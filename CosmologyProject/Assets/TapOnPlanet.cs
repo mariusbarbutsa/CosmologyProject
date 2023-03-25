130a3e13c63cf450f3edb22af155f972597c03b5
johannalakasz <johannalakasz@gmail.com>
2023-03-25 13:58:06 +0100

    state:planet

---
+++ CosmologyProject/Assets/TapOnPlanet.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
+
+public class TapOnPlanet : MonoBehaviour, IPointerClickHandler
+{
+    public PlanetState state = PlanetState.Norm;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("clicked");
+        state = PlanetState.Mars;
+
+    }
+
+    private void Update()
+    {
+        if (state == PlanetState.Mars)
+        {
+            Debug.Log("mars");
+
+            /* GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("Earth");
+            foreach (GameObject tagged in taggedObjects)
+            {
+                tagged.SetActive(false);
+            }
+            */
+            /*while (tag.Equals("Mars") == false)
+            {
+                gameObject.SetActive(false);
+            }
+            */
+        }
+    }
+
+    public enum PlanetState
+    {
+        Norm,
+        Mars,
+        Mercury,
+        Earth
+    }
+}
--- CosmologyProject/Assets/TouchManager.cs
+++ CosmologyProject/Assets/TouchManager.cs
@@ -3,22 +3,44 @@ using UnityEngine.InputSystem;
 
 public class TouchManager : MonoBehaviour
 {
-    public GameObject gameobject;
+    // public GameObject affectedGameObject;
     private PlayerInput playerInput;
 
     private InputAction touchPressAction;
+    private InputAction pressA;
+
+    // private string tag = "Mars";
 
     private void Awake() {
         playerInput = GetComponent<PlayerInput>();
         touchPressAction = playerInput.actions["TapOnPlanet"];
+        pressA = playerInput.actions["PressA"];
 
+    }
+
+    private void Start()
+    {
+        
+        
     }
 
     private void Update()
     {
         if (touchPressAction.WasPerformedThisFrame())
         {
-            Debug.Log("Heyy");
+           // Debug.Log("Heyy");
         }
+        if (pressA.WasPerformedThisFrame())
+        {
+            // Debug.Log("Huh");
+            // affectedGameObject.SetActive(false);
+            GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("Earth");
+            foreach (GameObject tagged in taggedObjects)
+            {
+                tagged.SetActive(false);
+            }
+        }
+
+        
     }
 }
